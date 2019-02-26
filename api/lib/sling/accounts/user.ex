2ee4a8db7603f91365635a58d2a48f41a695be1b
takethispie <takethispie@gmail.com>
2019-02-26 21:53:37 +0100

    add bcrypt  add password hashing

--- api/lib/sling/accounts/user.ex
+++ api/lib/sling/accounts/user.ex
@@ -5,16 +5,36 @@ defmodule Sling.Accounts.User do
 
   schema "users" do
     field :email, :string
-    field :password_hash, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
 
   @doc false
-  def changeset(user, attrs) do
-    user
-    |> cast(attrs, [:username, :email, :password_hash])
-    |> validate_required([:username, :email, :password_hash])
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:username)
+    |> unique_constraint(:email)
+  end
+
+  def registration_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+      _ ->
+        changeset
+    end
   end
 end
--- api/mix.exs
+++ api/mix.exs
@@ -41,7 +41,8 @@ defmodule Sling.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:comeonin, "~> 5.0"}
+      {:comeonin, "~> 5.0"},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
